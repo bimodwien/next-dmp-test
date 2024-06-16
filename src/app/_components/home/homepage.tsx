e52694e8f7111746d9d3d57e9668d2f0b17557d4
bimodwien <bimodwien2@gmail.com>
2024-06-16 13:34:11 +0700

    update: nyari API dulu ya

---
+++ src/app/_components/home/homepage.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+const Homepage = (props: Props) => {
+  const satu = 1;
+  return (
+    <>
+      <div>Homepage</div>
+    </>
+  );
+};
+
+export default Homepage;
---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <>
+      <div>Navbar</div>
+    </>
+  );
+};
+
+export default Navbar;
